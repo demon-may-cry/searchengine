a398e4992c1a6dc90a9e39bdc91b446a256c089a
Дмитрий Ельцов <demonmaycry@mail.ru>
2024-10-10 21:23:15 +0300

    A model has been added

---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,24 @@
+package searchengine.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "page", indexes = @Index(columnList = "path", name = "path_index", unique = true))
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "site_id", columnDefinition = "INT", nullable = false)
+    private Integer siteId;
+
+    @Column(name = "path", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String path;
+
+    @Column(name = "code", columnDefinition = "INT", nullable = false)
+    private Integer code;
+
+    @Column(name = "content", columnDefinition = "MEDIUMTEXT", nullable = false)
+    private String content;
+}
---
+++ src/main/java/searchengine/model/Site.java
@@ -0,0 +1,30 @@
+package searchengine.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "site")
+public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "ENUM('INDEXING', 'INDEXED', 'FAILED')", nullable = false)
+    private StatusType status;
+
+    @Column(name = "status_time", columnDefinition = "DATETIME", nullable = false)
+    private Date statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(name = "url", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String url;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String name;
+}
